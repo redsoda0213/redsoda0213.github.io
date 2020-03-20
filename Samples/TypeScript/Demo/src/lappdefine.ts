819c7c4e990c0405c99e96bafedae04486633914
BobYeh <bobgary2002@gmail.com>
2020-03-20 10:58:06 +0900

    init commit

---
+++ Samples/TypeScript/Demo/src/lappdefine.ts
@@ -0,0 +1,74 @@
+/**
+ * Copyright(c) Live2D Inc. All rights reserved.
+ *
+ * Use of this source code is governed by the Live2D Open Software license
+ * that can be found at https://www.live2d.com/eula/live2d-open-software-license-agreement_en.html.
+ */
+
+import { LogLevel } from '@framework/live2dcubismframework';
+
+/**
+ * Sample Appで使用する定数
+ */
+// 画面
+export const ViewMaxScale = 2.0;
+export const ViewMinScale = 0.8;
+//export const ViewMaxScale = 100.0;
+//export const ViewMinScale = 0.8;
+
+export const ViewLogicalLeft = -1.0;
+export const ViewLogicalRight = 1.0;
+
+export const ViewLogicalMaxLeft = -2.0;
+export const ViewLogicalMaxRight = 2.0;
+export const ViewLogicalMaxBottom = -2.0;
+export const ViewLogicalMaxTop = 2.0;
+
+// 相対パス
+export const ResourcesPath = '../../Resources/';
+
+// モデルの後ろにある背景の画像ファイル
+export const BackImageName = 'transparent2.png';
+
+// 歯車
+export const GearImageName = 'icon_gear.png';
+
+// 終了ボタン
+export const PowerImageName = 'CloseNormal.png';
+
+// モデル定義---------------------------------------------
+// モデルを配置したディレクトリ名の配列
+// ディレクトリ名とmodel3.jsonの名前を一致させておくこと
+export const ModelDir: string[] = ['擠奶妹07_胸部外擴'];
+export const ModelDirSize: number = ModelDir.length;
+
+// 外部定義ファイル（json）と合わせる
+export const MotionGroupIdle = 'Idle'; // アイドリング
+export const MotionGroupTapBody = 'TapBody'; // 体をタップしたとき
+
+// 外部定義ファイル（json）と合わせる
+export const HitAreaNameHead = 'Head';
+export const HitAreaNameBody = 'Body';
+
+// モーションの優先度定数
+export const PriorityNone = 0;
+export const PriorityIdle = 1;
+export const PriorityNormal = 2;
+export const PriorityForce = 3;
+
+// デバッグ用ログの表示オプション
+export const DebugLogEnable = true;
+export const DebugTouchLogEnable = false;
+
+// Frameworkから出力するログのレベル設定
+export const CubismLoggingLevel: LogLevel = LogLevel.LogLevel_Verbose;
+
+// デフォルトのレンダーターゲットサイズ
+//export const RenderTargetWidth = 1900;
+//export const RenderTargetHeight = 1000;
+
+// export const RenderTargetWidth = window.innerWidth - 20;
+// export const RenderTargetHeight = window.innerHeight - 20;
+
+export const RenderTargetWidth = document.documentElement.clientWidth;
+export const RenderTargetHeight = document.documentElement.clientHeight;
